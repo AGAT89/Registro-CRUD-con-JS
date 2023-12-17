1e1a932691fc17a3d5314777be94476215ece6df
AGAT89 <aariastevez@gmail.com>
2023-12-16 22:13:27 -0500

    Segunda Actualización

--- app.js
+++ app.js
@@ -2,72 +2,182 @@ let listaEmpleados = [];
 
 const objEmpleado = {
     id: '',
+    apellidoPaterno: '',
+    apellidoMaterno: '',
     nombre: '',
-    apellido: ''
-}
+    telefono: '',
+    email: '',
+};
 
 let editar = false;
 
-const formulario = document.querySelector('#formulario')
-const nombreInput = document.querySelector('#nombre')
-const apellidoInput = document.querySelector('#apellido')
-const btnEnviar = document.querySelector('#btnEnviar')
+const formulario = document.querySelector('#formulario');
+const apellidoPaternoInput = document.querySelector('#apellidoPaterno');
+const apellidoMaternoInput = document.querySelector('#apellidoMaterno');
+const nombreInput = document.querySelector('#nombre');
+const telefonoInput = document.querySelector('#telefono');
+const emailInput = document.querySelector('#email');
+const btnEnviar = document.querySelector('#btnEnviar');
 
+formulario.addEventListener('submit', validarFormulario);
 
-function validarFormulario(e){
-    //Funcion : event.prevenetDefault()
+function validarFormulario(e) {
+    //funcion : event.preventDefault()
     e.preventDefault();
 
-    if(nombreInput.value === '' || apellidoInput.value === ''){
-        alert('Por favor rellena todos los campos');
-        return;
+    if (apellidoPaternoInput.value === '' || apellidoMaternoInput.value === '' || nombreInput.value === '' || telefonoInput.value === '' || emailInput.value === '') {
+        alert('por favor ingresa todos los campos')
+        return
     }
 
-    agregandoPersona();
+    if (editar) {
+        editarPersona();
+        editar = false;
+    } else {
+        objEmpleado.id = Date.now();
+        objEmpleado.apellidoPaterno = apellidoPaternoInput.value;
+        objEmpleado.apellidoMaterno = apellidoMaternoInput.value;
+        objEmpleado.nombre = nombreInput.value;
+        objEmpleado.telefono = telefonoInput.value;
+        objEmpleado.email = emailInput.value;
+        agregandoPersona();
+    }
+
 }
 
-function agregandoPersona(){
-    //PUSH
-    listaEmpleados.push({...objEmpleado});
+function agregandoPersona() {
+    //PUSH - agregar valores
+    listaEmpleados.push({ ...objEmpleado });
+    //MOSTRAR
+    mostrarPersona();
+    formulario.reset();
+    limpiarObjetosPersona();
 }
 
-function mostrarPersona(){
-    //queryselector
-    const divEmpleado = document.querySelector('.divEmpleado');
+function limpiarObjetosPersona() {
+    objEmpleado.id = '';
+    objEmpleado.apellidoPaterno = '';
+    objEmpleado.apellidoMaterno = '';
+    objEmpleado.nombre = '';
+    objEmpleado.telefono = '';
+    objEmpleado.email = '';
+}
+
+function mostrarPersona() {
+    const divEmpleados = document.querySelector('.div-empleados');
+    
+    // Limpiar contenido anterior de la tabla
+    divEmpleados.innerHTML = '';
 
     listaEmpleados.forEach(persona => {
-        const {id, nombre, apellido} = persona
-        // 1 Fiorella Rodriguez
+        const { id, apellidoPaterno, apellidoMaterno, nombre, telefono, email } = persona;
+        
+        // Crear una nueva fila
+        const fila = document.createElement('tr');
 
-        const parrafo = document.createElement('p');
-        parrafo.textContent = `${id} - ${nombre} - ${apellido} -`
-        parrafo.dataset.id = id;
+        // Crear celdas y agregar datos
+        const celdaId = document.createElement('td');
+        celdaId.textContent = id;
+        fila.appendChild(celdaId);
 
-        const editarBoton = document.createElement('button')
-        editarBoton.onclick
-        cargarEmpleado()
-    
-    })
+        const celdaApellidoPaterno = document.createElement('td');
+        celdaApellidoPaterno.textContent = apellidoPaterno;
+        fila.appendChild(celdaApellidoPaterno);
+
+        const celdaApellidoMaterno = document.createElement('td');
+        celdaApellidoMaterno.textContent = apellidoMaterno;
+        fila.appendChild(celdaApellidoMaterno);
+
+        const celdaNombre = document.createElement('td');
+        celdaNombre.textContent = nombre;
+        fila.appendChild(celdaNombre);
+
+        const celdaTelefono = document.createElement('td');
+        celdaTelefono.textContent = telefono;
+        fila.appendChild(celdaTelefono);
+
+        const celdaEmail = document.createElement('td');
+        celdaEmail.textContent = email;
+        fila.appendChild(celdaEmail);
+
+        // Agregar botones de editar y eliminar
+        const celdaAcciones = document.createElement('td');
+
+        const editarBoton = document.createElement('button');
+        editarBoton.onclick = () => cargarEmpleado(persona);
+        editarBoton.textContent = 'Editar';
+        editarBoton.classList.add('btn', 'btn-Editar');
+        celdaAcciones.appendChild(editarBoton);
+
+        const eliminarBoton = document.createElement('button');
+        eliminarBoton.onclick = () => eliminarEmpleado(id);
+        eliminarBoton.textContent = 'Eliminar';
+        eliminarBoton.classList.add('btn', 'btn-Eliminar');
+        celdaAcciones.appendChild(eliminarBoton);
+
+        fila.appendChild(celdaAcciones);
+
+        // Agregar la fila a la tabla
+        divEmpleados.appendChild(fila);
+    });
 }
 
-function cargarEmpleado(persona){
-    const {id, nombre, apellido} = persona;
+function cargarEmpleado(persona) {
+    const { id, apellidoPaterno, apellidoMaterno, nombre, telefono, email } = persona;
+    apellidoPaternoInput.value = apellidoPaterno;
+    apellidoMaternoInput.value = apellidoMaterno;
     nombreInput.value = nombre;
-    apellidoInput.value = apellido;
+    telefonoInput.value = telefono;
+    emailInput.value = email;
     objEmpleado.id = id;
-    
-    const editarBoton = document.createElement;
-    editarBoton.onclick = () => cargarEmpleado;
-    editarBoton.textContent = 'Editar';
-    editarBoton.classList.add('btn','btn-editar')
-    editarBoton.append(editarBoton);
-    //
 
-    const eliminarEmpleado = document.createElement;
-    eliminarBoton.onclick = () => cargarEmpleado;
-    eliminarBoton.textContent = 'Editar';
-    eliminarBoton.classList.add('btn','btn-editar')
-    eliminarBoton.append(editarBoton);
-
-    
+    formulario.querySelector('button[type = "submit"]').textContent = 'Modificar';
+    editar = true;
 }
+
+function eliminarEmpleado(id) {
+    listaEmpleados = listaEmpleados.filter(empleado => empleado.id !== id);
+    mostrarPersona();
+}
+
+
+function editarPersona() {
+    objEmpleado.apellidoPaterno = apellidoPaternoInput.value;
+    objEmpleado.apellidoMaterno = apellidoMaternoInput.value;
+    objEmpleado.nombre = nombreInput.value;
+    objEmpleado.telefono = telefonoInput.value;
+    objEmpleado.email = emailInput.value;
+    
+    listaEmpleados = listaEmpleados.map(persona => {
+        if (persona.id === objEmpleado.id) {
+            return {
+                id: objEmpleado.id,
+                apellidoPaterno: objEmpleado.apellidoPaterno,
+                apellidoMaterno: objEmpleado.apellidoMaterno,
+                nombre: objEmpleado.nombre,
+                telefono: objEmpleado.telefono,
+                email: objEmpleado.email,
+            };
+        } else {
+            return persona;
+        }
+    });
+
+    mostrarPersona();
+    formulario.reset();
+    limpiarObjetosPersona();
+    
+    // Cambiar el texto del botón de "Modificar" a "Enviar" después de editar
+    formulario.querySelector('button[type="submit"]').textContent = 'Enviar';
+    editar = false;
+}
+
+
+
+
+
+
+
+
+
+
